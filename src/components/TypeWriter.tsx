0c7f06fb3b1aa6327488b371bf517989d121f4a3
codenprose <danielkhunter@gmail.com>
2025-04-05 00:11:28 -0700

    Remove unused props and imports from TypeWriter component

--- src/components/TypeWriter.tsx
+++ src/components/TypeWriter.tsx
@@ -1,15 +1,10 @@
 "use client"
 
-import Link from 'next/link'
-
 interface TypeWriterProps {
-  title: string
   blurb: string
-  author: string
-  link: string
 }
 
-export default function TypeWriter({ title, blurb, author, link }: TypeWriterProps) {
+export default function TypeWriter({ blurb }: TypeWriterProps) {
   return (
     <div className="px-4">
       <div className="w-[600px] min-h-[150px] font-mono flex">
