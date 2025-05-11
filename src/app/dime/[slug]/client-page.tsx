8ed91f9c1d1ff779ef83184fdbbf1b21d9284280
codenprose <danielkhunter@gmail.com>
2025-05-11 07:30:22 -0700

    Remove unused 'allStories' prop from Card component in ClientPage to streamline story handling.

--- src/app/dime/[slug]/client-page.tsx
+++ src/app/dime/[slug]/client-page.tsx
@@ -43,7 +43,6 @@ export default function ClientPage({ slug }: ClientPageProps) {
       />
       <Card 
         initialStory={story} 
-        allStories={stories}
         isFadingOut={false}
       />
 
